3df062d8aa8b20bb18970deb405ff08e5a99c8c1
Luciano Erick <luciano.erick.filho@ccc.ufcg.edu.br>
2019-09-03 09:40:11 -0300

    Counting Sort implementado!

--- R03-02-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
+++ R03-02-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
@@ -12,31 +12,42 @@ public class CountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-
-		// Descobrindo o maior elemento da sequencia.
-		int maior = 0;
-		for (int i = leftIndex; i < rightIndex; i++) {
-			if (array[i] > maior) {
-				maior = array[i];
+		if (array.length > 0) {
+			// Descobrindo o maior e menor elementos da sequencia.
+			int maior = array[leftIndex];
+			int menor = array[leftIndex];
+			for (int i = leftIndex; i <= rightIndex; i++) {
+				if (array[i] >= maior) {
+					maior = array[i];
+				}
+				if (array[i] < menor) {
+					menor = array[i];
+				}
 			}
-		}
 
-		Integer[] auxiliar = new Integer[maior];
+			int[] auxiliar = new int[maior - menor + 1];
+			// Registrando a frequencia com que um elemento aparece na sequencia.
+			for (int i = leftIndex; i <= rightIndex; i++) {
+				auxiliar[array[i] - menor] += 1;
+			}
 
-		// Registrando a frequencia com que um elemento aparece na sequencia.
-		for (int i = leftIndex; i < rightIndex; i++) {
-			auxiliar[array[i] - 1] += 1;
-		}
+			// Fazendo a soma cumulativa no array auxiliar.
+			for (int i = 1; i < auxiliar.length; i++) {
+				auxiliar[i] += auxiliar[i - 1];
+			}
 
-		// Fazendo a soma cumulativa no array auxiliar.
-		for (int i = 0; i < auxiliar.length; i++) {
-			auxiliar[i] += auxiliar[i - 1];
-		}
+			// criando um novo vetor, com o intuito de ordena-lo, utilizando do original e do
+			// vetor auxiliar.
+			Integer[] arrayOrdenado = new Integer[array.length];
+			for (int i = rightIndex; i >= leftIndex; i--) {
+				arrayOrdenado[auxiliar[array[i] - menor] - 1] = array[i];
+				auxiliar[array[i] - menor] -= 1;
+			}
 
-		Integer[] arrayOrdenado = new Integer[array.length];
-		for (int i = rightIndex; i >= leftIndex; i--) {
-			arrayOrdenado[auxiliar[array[i] - 1] - 1] = array[i];
-
+			// fazendo uma copia, para o vetor original, do vetor ordenado.
+			for (int i = leftIndex; i <= rightIndex; i++) {
+				array[i] = arrayOrdenado[i];
+			}
 		}
 	}
 }
