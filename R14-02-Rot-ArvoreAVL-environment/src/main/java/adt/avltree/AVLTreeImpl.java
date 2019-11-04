f8355f6da27aa49a1b34198b4739bcb7e4533b30
Luciano Erick <luciano.erick.filho@ccc.ufcg.edu.br>
2019-11-04 16:37:56 -0300

    Classe BSTImpl.java implementada.

---
+++ R14-02-Rot-ArvoreAVL-environment/src/main/java/adt/avltree/AVLTreeImpl.java
@@ -0,0 +1,42 @@
+package adt.avltree;
+
+import adt.bst.BSTImpl;
+import adt.bst.BSTNode;
+
+/**
+ * 
+ * Implementacao de uma arvore AVL
+ * 
+ * @author Claudio Campelo
+ *
+ * @param <T>
+ */
+public class AVLTreeImpl<T extends Comparable<T>> extends BSTImpl<T> implements
+		AVLTree<T> {
+	// TODO Do not forget: you must override the methods insert and remove
+	// conveniently.
+
+	private static final int ZERO = 0;
+	private static final int BALANCE_LEFT = 1;
+	private static final int BALANCE_RIGHT = -1;
+
+	// AUXILIARY
+	protected int calculateBalance(BSTNode<T> node) {
+		if (node == null || node.isEmpty()) {
+			return 0;
+		}
+		int height
+	}
+
+	// AUXILIARY
+	protected void rebalance(BSTNode<T> node) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	// AUXILIARY
+	protected void rebalanceUp(BSTNode<T> node) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+}
