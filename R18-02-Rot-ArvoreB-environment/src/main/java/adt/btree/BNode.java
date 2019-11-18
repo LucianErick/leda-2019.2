a5ff7751ad53f73e3cc04b9c9d3e7441f11fff62
Luciano Erick <luciano.erick.filho@ccc.ufcg.edu.br>
2019-11-18 16:30:10 -0300

    R18-Árvore B implementado.

--- R18-02-Rot-ArvoreB-environment/src/main/java/adt/btree/BNode.java
+++ R18-02-Rot-ArvoreB-environment/src/main/java/adt/btree/BNode.java
@@ -8,12 +8,13 @@ public class BNode<T extends Comparable<T>> {
 	protected LinkedList<BNode<T>> children; //PODERIA TRABALHAR COM ARRAY TAMBEM
 	protected BNode<T> parent;
 	protected int order;
-	
-	public BNode(int order){
+
+	public BNode(int order) {
 		this.order = order;
-		this.elements = new LinkedList<T>();	
+		this.elements = new LinkedList<T>();
 		this.children = new LinkedList<BNode<T>>();
 	}
+
 	@Override
 	public String toString() {
 		return this.elements.toString();
@@ -22,12 +23,12 @@ public class BNode<T extends Comparable<T>> {
 	@Override
 	public boolean equals(Object obj) {
 		boolean resp = false;
-		if(obj != null){
-			if(obj instanceof BNode){
-				if(this.size() == ((BNode<T>) obj).size()){
+		if (obj != null) {
+			if (obj instanceof BNode) {
+				if (this.size() == ((BNode<T>) obj).size()) {
 					resp = true;
 					int i = 0;
-					while(i<this.size() && resp){
+					while (i < this.size() && resp) {
 						resp = resp && this.getElementAt(i).equals(((BNode<T>) obj).getElementAt(i));
 						i++;
 					}
@@ -36,91 +37,114 @@ public class BNode<T extends Comparable<T>> {
 		}
 		return resp;
 	}
-	public boolean isEmpty(){
+
+	public boolean isEmpty() {
 		return this.size() == 0;
 	}
-	public int size(){
+
+	public int size() {
 		return this.elements.size();
 	}
-	public boolean isLeaf(){
+
+	public boolean isLeaf() {
 		return this.children.size() == 0;
 	}
-	public boolean isFull(){
-		return this.size()== order - 1;
+
+	public boolean isFull() {
+		return this.size() == order - 1;
 	}
-	public void addElement(T element){
+
+	public void addElement(T element) {
 		this.elements.add(element);
 		Collections.sort(elements);
 	}
-	public void removeElement(T element){
+
+	public void removeElement(T element) {
 		this.elements.remove(element);
 	}
-	public void removeElement(int position){
+
+	public void removeElement(int position) {
 		this.elements.remove(position);
 	}
-	public void addChild(int position, BNode<T> child){
+
+	public void addChild(int position, BNode<T> child) {
 		this.children.add(position, child);
 		child.parent = this;
 	}
-	public void removeChild(BNode<T> child){
+
+	public void removeChild(BNode<T> child) {
 		this.children.remove(child);
 	}
-	public int indexOfChild(BNode<T> child){
+
+	public int indexOfChild(BNode<T> child) {
 		return this.children.indexOf(child);
 	}
-	public T getElementAt(int index){
+
+	public T getElementAt(int index) {
 		return this.elements.get(index);
 	}
-	protected void split(){
+
+	protected void split() {
 		//TODO Implement your code here
 		throw new UnsupportedOperationException("Not implemented yet!");
 	}
-	protected void promote(){
+
+	protected void promote() {
 		//TODO Implement your code here
 		throw new UnsupportedOperationException("Not implemented yet!");
 	}
+
 	public LinkedList<T> getElements() {
 		return elements;
 	}
+
 	public void setElements(LinkedList<T> elements) {
 		this.elements = elements;
 	}
+
 	public LinkedList<BNode<T>> getChildren() {
 		return children;
 	}
+
 	public void setChildren(LinkedList<BNode<T>> children) {
 		this.children = children;
 	}
-	public BNode<T> copy(){
+
+	public BNode<T> copy() {
 		BNode<T> result = new BNode<T>(order);
 		result.parent = parent;
 		for (int i = 0; i < this.elements.size(); i++) {
 			result.addElement(this.elements.get(i));
 		}
 		for (int i = 0; i < this.children.size(); i++) {
-			result.addChild(i,((BNode<T>)this.children.get(i)).copy());
+			result.addChild(i, ((BNode<T>) this.children.get(i)).copy());
 		}
-		
+
 		return result;
 	}
+
 	public BNode<T> getParent() {
 		return parent;
 	}
+
 	public void setParent(BNode<T> parent) {
 		this.parent = parent;
 	}
+
 	public int getMaxKeys() {
 		return order - 1;
 	}
+
 	public int getMaxChildren() {
 		return order;
 	}
+
 	public int getOrder() {
 		return order;
 	}
+
 	public void setOrder(int order) {
 		this.order = order;
 	}
 
-	
 }
